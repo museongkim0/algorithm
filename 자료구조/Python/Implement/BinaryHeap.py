70c83b84f1da2657c7fcf39e110d7b8b7b35c3a0
museongkim <mstar0228@gmail.com>
2025-07-22 18:32:33 +0900

    바이너리 힙 구현 완료

--- 자료구조/Python/Implement/BinaryHeap.py
+++ 자료구조/Python/Implement/BinaryHeap.py
@@ -2,6 +2,7 @@ from typing import TypeVar, Optional
 from ADT.ADTBinaryHeap import ADTBinaryHeap
 from Implement.BinaryTree import PointerBinaryTree, Node
 from collections import deque
+from Implement.Comparable import TupleKey
 
 T = TypeVar('T')
 
@@ -9,28 +10,6 @@ class BinaryHeap(ADTBinaryHeap[T]):
     def __init__(self):
         super().__init__(PointerBinaryTree[T]())
         self.list = deque()
-
-    def __update_list(self):
-        root = self.tree.get_root()
-        self.list = deque()
-        if root is None:
-            return
-        level = 1
-        self.list.append([level, [root]])
-        while True:
-            if self.tree.is_leaf(self.list[level-1][1][0]):
-                return
-            level += 1
-            self.list.append([level, []])
-            for i in self.list[level-2][1]:
-                if i.get_left() is None:
-                    return
-                self.list[level-1][1].append(i.get_left())
-                if i.get_right() is not None:
-                    self.list[level-1][1].append(i.get_right())
-                else:
-                    return
-
 
     def push(self, data: T) -> None:
         # 새로운 값 삽입
@@ -53,27 +32,6 @@ class BinaryHeap(ADTBinaryHeap[T]):
             new_node = self.tree.get_right_child(parent_node)
         self.__percolate_up(new_node)
         self.list[-1][1].append(new_node)
-
-            # if self.tree.is_leaf(node):
-            #     self.__insert_node(node, data, "left")
-            #     return
-            # if self.tree.get_left_child(node) is not None and self.tree.get_right_child(node) is None:
-            #     self.__insert_node(node, data, "right")
-            #     return
-            # self.list.popleft()
-
-    # def __insert_node(self, node: Optional['Node[T]'], data: T, mode: str) -> None:
-    #     if mode == "right":
-    #         self.tree.insert_right_child(node, data)
-    #         new_node = self.tree.get_right_child(node)
-    #         self.__percolate_up(new_node)
-    #         self.list.append(new_node)
-    #         return
-    #     self.tree.insert_left_child(node, data)
-    #     new_node = self.tree.get_left_child(node)
-    #     self.__percolate_up(new_node)
-    #     self.list.append(new_node)
-    #     return
 
     def __percolate_up(self, node: Optional['Node[T]']) -> None:
         parent_node = node.get_parent()
@@ -114,7 +72,6 @@ class BinaryHeap(ADTBinaryHeap[T]):
     def pop(self) -> Optional[T]:
         # 최소값/최대값 제거 후 반환
         pop_node = self.list[-1][1][-1]
-        print(pop_node.get_data())
         root_node = self.tree.get_root()
         pop_val = root_node.get_data()
         if pop_node is root_node:
@@ -133,26 +90,6 @@ class BinaryHeap(ADTBinaryHeap[T]):
             self.list.pop()
         return pop_val
 
-    def __find_last(self) -> Node[T]:
-        bfs = deque()
-        root_node = self.tree.get_root()
-        bfs.append(root_node)
-        pop_node = None
-        while len(bfs) != 0:
-            pop_node = bfs.popleft()
-            left_child_node = self.tree.get_left_child(pop_node)
-            right_child_node = self.tree.get_right_child(pop_node)
-            if left_child_node is None and right_child_node is None:
-                continue
-            elif left_child_node is not None and right_child_node is None:
-                bfs.append(left_child_node)
-            elif left_child_node is not None and right_child_node is not None:
-                bfs.append(left_child_node)
-                bfs.append(right_child_node)
-        return pop_node
-
-
-
     def peek(self) -> Optional[T]:
         # 최소값/최대값 반환
         return self.tree.get_root().get_data()
@@ -168,33 +105,33 @@ class BinaryHeap(ADTBinaryHeap[T]):
     def display(self):
         return self.tree.display()
 
-# heap = BinaryHeap[int]()
-# print(heap.is_empty())
-# print(heap.get_size())
-# heap.push(303)
-# print(heap.display())
-# heap.push(159)
-# print(heap.display())
-# heap.push(7)
-# print(heap.display())
-# heap.push(6)
-# print(heap.display())
-# heap.push(20)
-# print(heap.display())
-# print(heap.is_empty())
-# print(heap.get_size())
-#
-# print(heap.pop())
-# print(heap.display())
-# heap.push(3)
-# print(heap.display())
-# print(heap.peek())
-# print(heap.pop())
-# print(heap.pop())
-# print(heap.pop())
-# print(heap.pop())
-# print(heap.pop())
-# print(heap.is_empty())
+heap = BinaryHeap[TupleKey[int, int]]()
+print(heap.is_empty())
+print(heap.get_size())
+heap.push(TupleKey((303,50)))
+print(heap.display())
+heap.push(TupleKey((159, 100)))
+print(heap.display())
+heap.push(TupleKey((7,9)))
+print(heap.display())
+heap.push(TupleKey((6,10)))
+print(heap.display())
+heap.push(TupleKey((20,5)))
+print(heap.display())
+print(heap.is_empty())
+print(heap.get_size())
+
+print(heap.pop())
+print(heap.display())
+heap.push(TupleKey((3, 100)))
+print(heap.display())
+print(heap.peek())
+print(heap.pop())
+print(heap.pop())
+print(heap.pop())
+print(heap.pop())
+print(heap.pop())
+print(heap.is_empty())
 
 
 
--- 자료구조/Python/Implement/BinaryTree.py
+++ 자료구조/Python/Implement/BinaryTree.py
@@ -191,11 +191,11 @@ class PointerBinaryTree(ADTBinaryTree[T]):
         self.__pre_order(node.right, answer)
 
     def display(self):
-        answer = []
-        self.__pre_order(self.root, answer)
-        # self.__in_order(self.root, answer)
-        return answer
-        # return self.print_subtree(self.root)
+        # answer = []
+        # self.__pre_order(self.root, answer)
+        # # self.__in_order(self.root, answer)
+        # return answer
+        return self.print_subtree(self.root)
 
     # 서브트리를 (간단하게) 출력하는 함수 (재귀)
     def print_subtree(self, node: Optional[Node], prefix: str = "", is_left: bool = True):
